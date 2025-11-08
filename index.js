96e02f2b5ee7c5200dadae0029b040541b26f74f
Ryan Vieira Assumpção <ryanvieiramail@gmail.com>
2025-11-07 22:27:24 -0300

    setando endpoints, nodemon e thunder client

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('olá mundo em node')
+const express = require("express")
+const app = express()
+
+
+
+app.get('/',function (req,res){
+    res.send('api')
+
+})
+
+app.get('/ptbr',function (req,res){
+    res.send('olá mundo')
+
+})
+
+app.get('/es',function (req,res){
+    res.send('hola')
+
+})
+
+app.get('/eg',function (req,res){
+    res.send('Hello World')
+
+})
+
+// app.get('/oi',function (req,res){
+//    res.send('hello world')    
+// })
+
+app.listen(3000)
