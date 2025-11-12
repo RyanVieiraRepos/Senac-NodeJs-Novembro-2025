909769b40208ff130894a1f70cc8ab3ae4498709
Ryan Vieira Assumpção <ryanvieiramail@gmail.com>
2025-11-11 22:31:30 -0300

    posting to an array

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-app.use(express.json)
+app.use(express.json())
 
 const marvel = ["Homem Aranha", "Capitã Marvel", "Homem de ferro"]
 
@@ -9,18 +9,21 @@ const marvel = ["Homem Aranha", "Capitã Marvel", "Homem de ferro"]
 
 app.post('/herois', function(req,res){
     let novoHeroi = req.body.nome
-    herois.push(novoHeroi)
+    marvel.push(novoHeroi)
     res.send('ok')
 })
+
+
+app.get('/herois', function (req, res) {
+    res.send(marvel)
+
+})
+
 
 
 app.get('/marvel/:id', function (req, res) { // pegando o do objeto req, o 
     const id = req.params.id
     res.send(marvel[id - 1])
-
-})
-app.get('/', function (req, res) {
-    res.send('api')
 
 })
 
